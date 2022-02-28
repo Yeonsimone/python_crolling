cac1712c1aa8b4fe4a787c1b3823243f59cd5ace
Yeonwoo Kim <simone0109241@gmail.com>
2022-02-27 18:13:16 -0600

    download

--- selenium/google.py
+++ selenium/google.py
@@ -8,14 +8,38 @@ driver.get("https://www.google.com/imghp?hl=en&tab=ri&authuser=0&ogbl")
 elem = driver.find_element_by_name("q")
 elem.send_keys("cat")
 elem.send_keys(Keys.RETURN)
-driver.find_elements_by_css_selector(".rg_i.Q4LuWd")[0].click()
-time.sleep(3)
-imgURL = driver.find_element_by_css_selector(".n3VNCb").get_attribute("src")
-image = urllib.request.urlretrieve(imgURL, "test. jpg")
-# assert "Python" in driver.title
-# elem = driver.find_element_by_name("q")
-# elem.clear()
-# elem.send_keys("pycon")
-# elem.send_keys(Keys.RETURN)
-# assert "No results found." not in driver.page_source
-# driver.close()
+
+SCROLL_PAUSE_TIME = 1
+
+last_height = driver.execute_script("return document.body.scrollHeight")
+
+while True:
+    # Scroll down to bottom
+    driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+    # Wait to load page
+    time.sleep(SCROLL_PAUSE_TIME)
+
+    # Calculate new scroll height and compare with last scroll height
+    new_height = driver.execute_script("return document.body.scrollHeight")
+    if new_height == last_height:
+        try:
+            driver.find_element_by_css_selector(".mye4qd").click()
+        except:
+            break
+    last_height = new_height
+# 작은 이미지들 다운로드
+images = driver.find_elements_by_css_selector(".rg_i.Q4LuWd")
+count = 1
+for image in images:
+    try:
+        image.click()
+        time.sleep(3)
+        imgURL = driver.find_element_by_css_selector(
+            ".n3VNCb").get_attribute("src")
+        image = urllib.request.urlretrieve(imgURL, + str(count) + ". jpg")
+        count = count + 1
+    except:
+        pass
+
+driver.close()
